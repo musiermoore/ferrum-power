da4f850b2098166a3bfc111d22ee31db190bc145
musiermoore <musiermoore@gmail.com>
2021-07-15 19:43:42 +0600

    Created Order, OrderStatus, OrderProduct model, controller and migration

---
+++ database/migrations/2021_07_15_151611_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('description');
+            $table->string('address');
+
+            $table->integer('order_price')->default(0);
+
+            $table->foreignId('order_status_id');
+            $table->foreignId('operator_id');
+
+            $table->timestamps();
+
+            $table->foreign('order_status_id')->references('id')->on('order_statuses');
+            $table->foreign('operator_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2021_07_15_152411_create_order_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->foreignId('order_id');
+            $table->foreignId('product_id');
+
+            $table->integer('quantity');
+            $table->integer('full_price'); // price_for_one * quantity
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
