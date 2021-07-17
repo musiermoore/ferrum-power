17d4ce27e37c55c89e344f4125d2775c675d1c5a
musiermoore <musiermoore@gmail.com>
2021-07-17 10:26:38 +0600

    Created requests for Product

---
+++ app/Http/Requests/ProductCreateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'category_id'           => ['required', 'exists:category_products,id'],
+            'title'                 => ['required', 'string', 'min:2', 'max:128', 'unique:products,title'],
+            'slug'                  => ['string', 'min:2', 'max:128', 'unique:products,slug'],
+            'description'           => ['string', 'max:255'],
+            'image_path'            => ['string'],
+            'stock_availability'    => ['required', 'boolean'],
+            'price'                 => ['required', 'integer'],
+        ];
+    }
+}
