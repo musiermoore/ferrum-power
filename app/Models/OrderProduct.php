ed3b3fd3baa3f874886e64793fbf8abe8d46e28d
musiermoore <musiermoore@gmail.com>
2021-07-17 15:42:14 +0600

    Created methods for add a product and remove a product in Order

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -10,4 +10,21 @@ class OrderProduct extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    public static function addProductToOrder($order, $products)
+    {
+        foreach ($products as $product) {
+            $productPrice = Product::getProductPrice($product["product_id"]);
+
+            $order->products()->attach(array($product["product_id"] => [
+                'quantity'   => $product["quantity"],
+                'full_price' => $product["quantity"] * $productPrice,
+            ]));
+        }
+    }
+
+    public static function removeProductFromOrder($order, $product)
+    {
+        $order->products()->wherePivot('product_id', $product["product_id"])->detach();
+    }
 }
