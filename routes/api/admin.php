c00e8925717c890785f72f6885aeb48d712b92f1
musiermoore <musiermoore@gmail.com>
2021-07-19 16:03:56 +0600

    Added route names

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -18,16 +18,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
-Route::get('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:api');
+Route::get('/user/login', [\App\Http\Controllers\Api\AuthController::class, 'login'])
+    ->name('user.register');;
+Route::get('/user/logout', [\App\Http\Controllers\Api\AuthController::class, 'login'])
+    ->middleware('auth:api')
+    ->name('user.logout');;
 
 /*
  * Admin's routes
  */
 Route::group(['middleware' => ['role:admin']], function () {
-    Route::get('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
-    Route::patch('/operator/change', [\App\Http\Controllers\Api\Admin\OrderController::class, 'changeOrderOperator'])
-        ->prefix('/orders/{orderId}');
+    Route::get('/user/register', [\App\Http\Controllers\Api\AuthController::class, 'register'])->name('user.register');
+    Route::group(['prefix' => '/orders/{orderId}'], function () {
+        Route::patch('/operator/change', [\App\Http\Controllers\Api\Admin\OrderController::class, 'changeOrderOperator'])
+            ->name('orders.operator.change');
+    });
 });
 
 /*
@@ -42,9 +47,10 @@ Route::group(['middleware' => ['role:admin|operator']], function () {
         Route::apiResource('/products', \App\Http\Controllers\Api\Admin\OrderProductController::class, ['parameters' => [
             'products' => 'productId',
         ]])->except('show');
-        Route::patch('/operator/set', [\App\Http\Controllers\Api\Admin\OrderController::class, 'setOperatorToOrder']);
-        Route::patch('/operator/unset', [\App\Http\Controllers\Api\Admin\OrderController::class, 'unsetOperatorToOrder']);
-        Route::patch('/operator/change', [\App\Http\Controllers\Api\Admin\OrderController::class, 'changeOrderOperator']);
+        Route::patch('/operator/set', [\App\Http\Controllers\Api\Admin\OrderController::class, 'setOperatorToOrder'])
+            ->name('orders.operator.set');;
+        Route::patch('/operator/unset', [\App\Http\Controllers\Api\Admin\OrderController::class, 'unsetOperatorToOrder'])
+            ->name('orders.operator.unset');;
     });
 });
 
