0bfcc8109eda041d228faa1485ab84c0077cbd96
musiermoore <musiermoore@gmail.com>
2021-10-02 16:48:24 +0600

    Updated api order route

--- routes/api/user.php
+++ routes/api/user.php
@@ -11,6 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/categories', \App\Http\Controllers\Api\CategoryProductController::class)->only(['index', 'show']);
 Route::apiResource('/products', \App\Http\Controllers\Api\ProductController::class)->only(['index', 'show']);
-Route::apiResource('/orders', \App\Http\Controllers\Api\ProductController::class)->only(['store']);
+Route::apiResource('/orders', \App\Http\Controllers\Api\OrderController::class)->only(['store']);
 
 
